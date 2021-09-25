9939761e84c663404a315f835f260bb7d87d378f
Alexandre TITEUX <alexandre.titeux@epitech.eu>
2021-09-25 23:18:51 +0200

    Adding .env

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,17 @@
+import { env } from "process";
+
 export var json = {
     /* Epitech API key */
-    token: "auth-317d027bf6ca8eef363128780b10511bd56d8827",
+    token: env.token || "auth-14jan78076e6fb139498149e2bcef03a75c6b12",
 
 
     /* Refresh time in ms */
-    refreshTime: 60000,
+    refreshTime: env.refreshTime || 60000,
 
 
     /* Send invitation link to teammate */
-    sendInvitationLink: true,
+    sendInvitationLink: env.sendInvitationLink || true,
 
     /* You're epitech email (useless if sendInvitationLink is false)*/
-    epitechEmail: "alexandre.titeux@epitech.eu"
+    epitechEmail: env.epitechEmail || "alexandre.titeux@epitech.eu"
 }
